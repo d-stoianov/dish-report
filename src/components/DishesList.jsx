c80e119e83a387daad35af8ffb2ef62d5b2cd5a0
dmitriy <dima22022005@gmail.com>
2023-08-24 17:26:13 +0200

    Edit DishesList.jsx.
    Add onClick event on arrow. When hovering on the tbody, it changes its background color. By default isActive is set to true On mobile devices table requires full width and on desktop devices width is 500px. Everything is being centered.

--- src/components/DishesList.jsx
+++ src/components/DishesList.jsx
@@ -3,42 +3,43 @@ import { AiFillCaretDown, AiFillCaretLeft } from "react-icons/ai"
 
 const DishesList = ({ dishes }) => { // TODO: rename -> DishOverviewItem
     return (
-        <ul className="ml-6 mt-2">
+        <ul className="m-4">
             {dishes.map(dish => {
-                const [isActive, setIsActive] = useState(false)
+                const [isActive, setIsActive] = useState(true)
                 return (
-                    <li key={dish.id} className="my-4">
-                        <div
-                            className="flex items-center gap-2" 
-                        >
-                            <h1 
-                                onClick={() => setIsActive(!isActive)}
-                                className="text-xl font-semibold cursor-pointer"
+                    <li key={dish.id} className="my-4 flex justify-center">
+                        <div className="w-[500px]">
+                            <div
+                                className="flex items-center gap-2" 
                             >
-                                {dish.name}
-                            </h1>
-                            <div>
-                                {isActive ? <AiFillCaretDown /> : < AiFillCaretLeft />}
+                                <h1 
+                                    onClick={() => setIsActive(!isActive)}
+                                    className="text-xl font-semibold cursor-pointer"
+                                >
+                                    {dish.name}
+                                </h1>
+                                <div className="cursor-pointer" onClick={() => setIsActive(!isActive)}>
+                                    {isActive ? <AiFillCaretDown /> : < AiFillCaretLeft />}
+                                </div>
                             </div>
-                        </div>
-                        {isActive && 
-                        <table>
-                            <thead>
-                                <tr>
-                                    <td>City</td>
-                                    <td>Price</td>
-                                </tr>
-                            </thead>
-                            {dish.primeCosts.map(el => (
-                                <tbody>
+                            {isActive && 
+                            <table>
+                                <thead>
                                     <tr>
-                                        <td>{el.place}</td>
-                                        <td>{el.primeCost} %</td>
+                                        <td>City</td>
+                                        <td>Price</td>
                                     </tr>
+                                </thead>
+                                <tbody>
+                                    {dish.primeCosts.map(el => (
+                                            <tr>
+                                                <td>{el.place}</td>
+                                                <td>{el.primeCost} %</td>
+                                            </tr>
+                                    ))}
                                 </tbody>
-                            ))}
-                        </table>
-                        }
+                            </table>}
+                        </div>
                     </li>
                 )
             })}
