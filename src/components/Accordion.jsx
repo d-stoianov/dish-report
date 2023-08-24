adaa3103779ee7a5a37dc5d77f4415386fbf844d
dmitriy <dima22022005@gmail.com>
2023-08-24 23:19:19 +0200

    Create Accordion.jsx component.
    Also edit width to rem values.

---
+++ src/components/Accordion.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react"
+import { AiFillCaretDown, AiFillCaretLeft } from "react-icons/ai"
+
+const Accordion = ({title, children, defaultActive}) => {
+    const [isActive, setIsActive] = useState(defaultActive ?? true)
+    return (
+        <li className="my-4 flex justify-center">
+            <div className="w-full md:w-[32rem]">
+                <div className="flex items-center gap-2">
+                    <h1 
+                        onClick={() => setIsActive(!isActive)}
+                        className="text-xl font-semibold cursor-pointer"
+                    >
+                        {title}
+                    </h1>
+                    <div className="cursor-pointer" onClick={() => setIsActive(!isActive)}>
+                        {isActive ? <AiFillCaretDown /> : < AiFillCaretLeft />}
+                    </div>
+                </div>
+                {
+                isActive && children
+                }
+            </div>
+        </li>
+    )
+}
+
+export default Accordion
