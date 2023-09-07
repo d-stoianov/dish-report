f27e0cbef84bb843674105ceb7dd3be49bdc57e1
dmitriy <dima22022005@gmail.com>
2023-09-07 21:47:43 +0200

    Add loader spinner to dish details page.

--- src/pages/DishDetails.jsx
+++ src/pages/DishDetails.jsx
@@ -2,16 +2,22 @@ import React, { useEffect, useState } from "react"
 import { Link, useParams } from "react-router-dom"
 import { BiSolidChevronLeft } from "react-icons/bi"
 import { motion } from "framer-motion"
+import Loader from "@/components/Loader"
 import DishReportService from "@/services/DishReportService"
 
 const DishDetails = ({ selectedDish }) => {
     const { department, id } = useParams()
     const service = new DishReportService()
     const [dishDetails, setDishDetails] = useState([{}])
+    const [isLoading, setIsLoading] = useState(false)
+
     useEffect(() => {
+        setIsLoading(true)
         service.getDishDetails(department, id)
         .then(res => setDishDetails(res))
         .catch(err => console.log(err))
+        .finally(() => setIsLoading(false))
+
     }, [])
 
     return (
@@ -22,30 +28,34 @@ const DishDetails = ({ selectedDish }) => {
             animate={{opacity: 1}}
             exit={{opacity: 0}}
         >
-            <div className="flex justify-center items-center gap-3">
-                <Link to="/dish">
-                    <BiSolidChevronLeft />
-                </Link>
-                <h1 className="text-xl font-semibold">
-                    {selectedDish.name}
-                </h1>
-            </div>
-                <table className="w-full md:w-[70%]">
-                    <thead>
-                        <tr>
-                            <td>Ingredient</td>
-                            <td>Cost</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {dishDetails.map((ingredient, idx) => (
-                            <tr key={idx}>
-                                <td>{ingredient.name}</td>
-                                <td>?</td>
+            {
+                isLoading ? <Loader /> : <>
+                <div className="flex justify-center items-center gap-3">
+                    <Link to="/dish">
+                        <BiSolidChevronLeft />
+                    </Link>
+                    <h1 className="text-xl font-semibold">
+                        {selectedDish}
+                    </h1>
+                </div>
+                    <table className="w-full md:w-[70%]">
+                        <thead>
+                            <tr>
+                                <td>Ingredient</td>
+                                <td>Cost</td>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {dishDetails.map((ingredient, idx) => (
+                                <tr key={idx}>
+                                    <td>{ingredient.name}</td>
+                                    <td>?</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </>
+            }
         </motion.div>
     )
 }
